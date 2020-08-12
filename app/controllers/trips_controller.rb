bc1db1d734990b528ad6d5b46362c402ce3cc30e
Daniel Park <parkdk96@gmail.com>
2020-08-11 22:53:39 -1000

    trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,44 @@
 class TripsController < ApplicationController
+  skip_before_action :authorized
+
+  def index
+    trips = Trip.all
+    render :json => trips
+  end
+
+  def show
+    trip = Trip.find(params[:id])
+  end
+
+  def new
+    trip = Trip.new
+  end
+
+  def create
+    trip = Trip.new(trip_params)
+    trip.save
+    render :json -> trip
+  end
+
+  def edit
+    trip = Trip.find(params[:id])
+  end
+
+  def update
+    trip = Trip.find(params[:id])
+    trip.update(trip_params)
+    render :json => trip
+  end
+
+  def destroy
+    trip = Trip.find(params[:id])
+    trip.destroy
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:user_id, :country_id, :favorite, :comment)
+  end
+
 end
