620b32b0c634f1f111790aa14074e47acf7604c7
Daniel Park <parkdk96@gmail.com>
2020-08-11 22:56:15 -1000

    model relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :trips
+  has_many :countries, :through :trips
+
   validates :username, uniqueness: { case_sensitive: false }
 end
